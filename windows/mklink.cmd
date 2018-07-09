df4f1c609dd3ebd87a14040184f75ec9eb905055
Bruce <texasbruce@users.noreply.github.com>
2018-07-09 12:03:39 -0400

    Create mklink.cmd

---
+++ windows/mklink.cmd
@@ -0,0 +1,7 @@
+mklink /J C:\Users\bruce\.docker D:\.docker
+mklink /J C:\Users\bruce\.coursier D:\.coursier
+mklink /J C:\Users\bruce\.ivy2 D:\.ivy2
+mklink /J C:\Users\bruce\.m2 D:\.m2
+mklink /J C:\Users\bruce\.sbt D:\.sbt
+mklink /J C:\Users\bruce\.ssh D:\.ssh
+mklink /J "C:\Users\bruce\VirtualBox VMs" "D:\VirtualBox VMs"
