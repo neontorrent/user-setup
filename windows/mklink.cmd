d92165e8bd5405a57ecba73bf146c50ac2091c6c
Bruce <texasbruce@users.noreply.github.com>
2018-07-09 12:05:17 -0400

    Update mklink.cmd

--- windows/mklink.cmd
+++ windows/mklink.cmd
@@ -6,3 +6,4 @@ mklink /J C:\Users\bruce\.sbt D:\.sbt
 mklink /J C:\Users\bruce\.ssh D:\.ssh
 mklink /J "C:\Users\bruce\VirtualBox VMs" "D:\VirtualBox VMs"
 mklink /J C:\home D:\
+REM mklink /J C:\Sandbox D:\Sandbox
